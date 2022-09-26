df209dd339ebe0dead09fd950be2b3064ec4816c
Magna Nordgard <mnordgard@protonmail.com>
2022-09-26 19:47:47 +0200

    Section 2.1 - vending machine progress + removed smaller readme.md files for each section. Will rather mantain one master readme file.

--- Module 2. Object-Oriented Programming/1. Classes and Objects/vending-machine/Machine.java
+++ Module 2. Object-Oriented Programming/1. Classes and Objects/vending-machine/Machine.java
@@ -1,10 +1,48 @@
+import java.util.Arrays;
+
 // The vending machine has 9 different items arranged in 3 rows
 // Each spot in the machine shows the name, price, and quantity of the item.
 // The vending machine dispenses an item if its quantity is bigger than zero.
 
 public class Machine {
     
-    
+    private Item[][] items;
+
+    // Constructor method
+    public Machine(Item[][] items){
+        this.items = new Item[items.length][items[0].length];
+
+        for(int i=0; i<items.length; i++){              // Loop through rows
+            for(int j=0; j<items[i].length;j++){        // Loop through columns
+                this.items[i][j] = new Item(items[i][j]);
+            }
+        }
+    }
+
+    // Getters
+    public Item getItem(int row, int spot) {
+        return new Item(this.items[row][spot]);
+    }
+    // Setters
+    public void setItem(Item item, int row, int spot) {
+        this.items[row][spot] = new Item(item);
+    }
+
+    // toString
+    public String toString() {
+
+        String temp = "";
+        for (int i = 0; i < items.length; i++) {
+            temp += "\t";
+            for (int j = 0; j < items[i].length; j++) {
+                temp += items[i][j].toString() + " ";
+            }
+            temp += "\n\n";
+        }
+        temp += "\t************************************************";
+        return temp;
+    }
+    // Actions
     
     /**
      * Function name – dispense
@@ -17,4 +55,12 @@ public class Machine {
      *      • if so: decreases its quantity by one and returns true.
      *      • otherwise: returns false.
      */
+
+    public boolean dispense(int row, int spot) {
+        if (this.items[row][spot].getQuantity() > 0) {
+            items[row][spot].setQuantity(items[row][spot].getQuantity() - 1);
+            return true;
+        } 
+        return false;
+    }
 }
--- Module 2. Object-Oriented Programming/1. Classes and Objects/vending-machine/Main.java
+++ Module 2. Object-Oriented Programming/1. Classes and Objects/vending-machine/Main.java
@@ -1,26 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
+    
+    Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
 
         System.out.println("\t************************************************");
         System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
         System.out.println("\t************************************************");
         
-        /* Item[][] items = new Item[][] {
-        /     { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
-        /     { new Item("Fanta", 1.99, 2) , new Item("Barqs", 1.49, 2), new Item("A & W", 2.49, 3) },
-        /     { new Item("Crush", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
-        / };
-        */
-
-        Item[] items = new Item[] {
-            new Item("Fanta", 1.99, 2), 
-            new Item("Barqs", 1.49, 2), 
-            new Item("A & W", 2.49, 3)
+         Item[][] items = new Item[][] {
+             { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
+             { new Item("Fanta", 1.99, 2) , new Item("Barqs", 1.49, 2), new Item("A & W", 2.49, 3) },
+             { new Item("Crush", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
         };
+        
+        Machine machine = new Machine(items);
 
-        System.out.println(items[0].toString());
-        System.out.println(items[1].toString());
-        System.out.println(items[2].toString());
+        System.out.println();
+
+ 
 
     }
 }
