f44b3f4cc3e91bc61303ab04fee81e2bb8ebb63b
Magna Nordgard <mnordgard@protonmail.com>
2022-09-26 14:49:10 +0200

    Section 2.2 - airline-reservation-system pt. 2 complete

--- Module 2. Object-Oriented Programming/1. Classes and Objects/airline-reservation-system/Main.java
+++ Module 2. Object-Oriented Programming/1. Classes and Objects/airline-reservation-system/Main.java
@@ -3,19 +3,9 @@ import java.util.Arrays;
 public class Main {
   
     public static void main(String[] args) {
-        Person person = new Person();
+        Person person = new Person("Magna Nordgard","Norwegian","23/09/1992",5);
 
-        person.name = "Magna Nordgard";
-        person.nationality = "Norwegian";
-        person.dateOfBirth = "23/09/1992";
-        person.passport = new String[]{person.name, person.nationality, person.dateOfBirth};
-        person.seatNumber = 5;
-
-        System.out.println(person.name);
-        System.out.println(person.nationality);
-        System.out.println(person.dateOfBirth);
-        System.out.println(Arrays.toString(person.passport));
-        System.out.println(person.seatNumber);
+        person.printPersonData();
       
     }
   
--- Module 2. Object-Oriented Programming/1. Classes and Objects/airline-reservation-system/Person.java
+++ Module 2. Object-Oriented Programming/1. Classes and Objects/airline-reservation-system/Person.java
@@ -5,4 +5,20 @@ public class Person {
     String[] passport;
     int seatNumber;
 
+    // Constructor Method
+    public Person(String name, String nationality, String dateOfBirth, int seatNumber){
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.seatNumber = seatNumber;
+    }
+
+    // Print Person Data
+
+    public void printPersonData(){
+        System.out.println( "Name: "            + this.name         + "\n" + 
+                            "Nationality: "     + this.nationality  + "\n" + 
+                            "Date of Birth: "   + this.dateOfBirth  + "\n" + 
+                            "Seat Number: "     + this.seatNumber   + "\n");
+    }
 }
