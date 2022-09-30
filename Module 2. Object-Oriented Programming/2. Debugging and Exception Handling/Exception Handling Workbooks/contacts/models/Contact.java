b0e4de710fa6e0d9be76a2f8d70ed645c3c6ce1c
Magna Karina M. Nordgard <mnordgard@protonmail.com>
2022-09-30 14:57:45 +0200

    Section 2.2 - contacts workbook pt. 2 complete

--- Module 2. Object-Oriented Programming/2. Debugging and Exception Handling/Exception Handling Workbooks/contacts/models/Contact.java
+++ Module 2. Object-Oriented Programming/2. Debugging and Exception Handling/Exception Handling Workbooks/contacts/models/Contact.java
@@ -35,6 +35,57 @@ public class Contact {
         this.birthDate = birthDate;
         this.age = calculateAge(birthDate);
     }
+
+    
+    public Contact(Contact source) {
+        this.name = source.name;
+        this.phoneNumber = source.phoneNumber;
+        this.birthDate = source.birthDate;
+        this.age = source.age;
+    }
+    public String getName() {
+        return name;
+    }
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Contact name cannot be null/blank.");
+        }
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber == null || phoneNumber.isBlank()) {
+            throw new IllegalArgumentException("Contact phoneNumber cannot be null/blank.");
+        }
+        if (phoneNumber.length() < 5) {
+            throw new IllegalArgumentException("Contact phoneNumber cannot be less than 5 characters.");
+        }
+        
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setBirthDate(String birthDate) throws ParseException {
+        this.birthDate = birthDate;
+        setAge(calculateAge(birthDate));
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+
   
     public int calculateAge(String birthDate) throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
@@ -44,10 +95,10 @@ public class Contact {
     }
 
     public String toString() {
-        return ("Name: "             + this.name            + "\n" +
-                "Phone number: "    + this.phoneNumber      + "\n" +
-                "Birth Date: "      + this.birthDate        + "\n" +
-                "Age: "             + this.age + " years old.  \n");
+        return ("Name: \t\t"          + this.name               + "\n" +
+                "Phone number: \t"    + this.phoneNumber        + "\n" +
+                "Birth Date: \t"      + this.birthDate          + "\n" +
+                "Age: \t\t"           + this.age + " years old.    \n");
     }
 
 
