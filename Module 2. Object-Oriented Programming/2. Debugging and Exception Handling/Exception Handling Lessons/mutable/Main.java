aacaaac3c484375ea3d37d58bb943de9c10c61ef
Magna Karina M. Nordgard <mnordgard@protonmail.com>
2022-09-30 12:32:56 +0200

    Adding asset for readme.md ++ some changes in 2.2 lessons

--- Module 2. Object-Oriented Programming/2. Debugging and Exception Handling/Exception Handling Lessons/mutable/Main.java
+++ Module 2. Object-Oriented Programming/2. Debugging and Exception Handling/Exception Handling Lessons/mutable/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Car car = new Car("Nissan", 10000); //Mutable or Immutable?
+
+        // Another name for "setter" is mutator
+        // It follows that objects with setter methods are mutable objects
+        // -> Their value can be changed after creation
+
+        car.setMake("Honda");
     }
 }
