70db3799ccf849b99132de3d61f582396c6b6d71
Magna Karina M. Nordgard <mnordgard@protonmail.com>
2022-10-09 20:07:48 +0200

    Section 2.3 - Workbook: shopping-cart finished - pt. 4-5 completed

--- Module 2. Object-Oriented Programming/3. Unit Testing, Stream API and Lambda Expressions/Workbooks/shopping-cart/src/main/models/Cart.java
+++ Module 2. Object-Oriented Programming/3. Unit Testing, Stream API and Lambda Expressions/Workbooks/shopping-cart/src/main/models/Cart.java
@@ -1,9 +1,13 @@
 package src.main.models;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.text.DecimalFormat;
+
 
 public class Cart {
     ArrayList<Item> items;
+    private static final double TAX_RATE = 0.13;
 
     public Cart() {
         this.items = new ArrayList<Item>();
@@ -33,6 +37,14 @@ public class Cart {
         return true;
     }
 
+    /**
+    * Function name: remove
+    * @param name
+    * 
+    * Inside the function:
+    *   1. loop runs through the size of the ArrayList.
+    *   2. removes the item that matches the name passed in. 
+    */
     public void remove(String name){
         if(!(contains(name))){
             throw new IllegalStateException("Cannot remove an item that has not been added to the cart");
@@ -48,7 +60,63 @@ public class Cart {
     public boolean contains(Item item){
         return this.items.stream().anyMatch(sItem -> sItem.equals(item));
     }
+
+    /**
+    * Function name: getSubtotal
+    * @return double
+    * 
+    * Inside the function:
+    *   1. loop runs through the size of the ArrayList.
+    *   2. adds the price field of each Item object. 
+    *   3. returns the sum
+    */
+    public double getSubtotal(){
+        return this.items.stream().collect(Collectors.summingDouble(Item::getPrice)); 
+        
+        // Alternative from solution:
+        // return this.items.stream().mapToDouble((item) -> item.getPrice()).sum();
+    }
+
+    /**
+    * Function name: getTax
+    * @param double (subtotal)
+    * @return double
+    * 
+    * Inside the function:
+    *   1. calculates tax
+    *   2. rounds to two decimal places
+    *   3. returns the result
+    */
+    public double getTax(double subtotal){
+        DecimalFormat formatter = new DecimalFormat("#.##");
+        return Double.parseDouble(formatter.format(subtotal*TAX_RATE));
+    }
+
+    /**
+    * Function name: getTotal
+    * @param double (subtotal)
+    * @param double (tax)
+    * @return (double)
+    * 
+    * Inside the function:
+    *   1. returns subtotal + tax
+    */
+    public double getTotal(double subtotal, double tax){
+        return (subtotal + tax);
+    }
+
     
+    public String checkout(){
+        if(this.items.isEmpty()){
+            throw new IllegalStateException("Cannot checkout empty cart!");
+        }
+
+        return ("\tRECEIPT\n\n" +
+                "\tSubtotal: \t$" + getSubtotal()                                   + "\n" +
+                "\tTax:      \t$" + getTax(getSubtotal())                           + "\n" +
+                "\tTotal:    \t$" + getTotal(getSubtotal(), getTax(getSubtotal()))  + "\n");
+    }
+
     public String toString() {
         String temp = "";
         for (int i = 0; i < this.items.size(); i++) {
--- Module 2. Object-Oriented Programming/3. Unit Testing, Stream API and Lambda Expressions/Workbooks/shopping-cart/src/test/CartTest.java
+++ Module 2. Object-Oriented Programming/3. Unit Testing, Stream API and Lambda Expressions/Workbooks/shopping-cart/src/test/CartTest.java
@@ -5,21 +5,32 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.DecimalFormat;
+
 import src.main.models.*;
 
 public class CartTest {
 
     Cart cart;
+
+    static DecimalFormat formatter = new DecimalFormat("#.##");
+
     static Item testItem1 = new Item("Pepsi", 1.99);
     static Item testItem2 = new Item("Crush", 1.99);
     static Item testItem3 = new Item("Solo", 5.99);
+    
+    static final double TAX_RATE = 0.13;
 
+    static double expectedSubtotal = testItem1.getPrice() + testItem2.getPrice();
+    static double expectedTax = Double.parseDouble(formatter.format(expectedSubtotal*TAX_RATE));
+    static double expectedTotal = expectedSubtotal + expectedTax;
 
     @Before
     public void setup(){
         cart = new Cart();
         cart.add(testItem1);
         cart.add(testItem2);
+        
     }
 
     @Test
@@ -42,6 +53,28 @@ public class CartTest {
     @Test(expected = IllegalStateException.class)
     public void removeItemNotInCartTest(){
         cart.remove(testItem3.getName());
+    }
+
+    @Test
+    public void subtotalCalculationTest(){
+        assertTrue(expectedSubtotal == cart.getSubtotal());
+    }
+
+    @Test
+    public void taxCalculationTest(){
+        assertTrue(expectedTax == cart.getTax(cart.getSubtotal()));
+    }
+
+    @Test
+    public void totalCalculationTest(){
+        assertTrue(expectedTotal == cart.getTotal(cart.getSubtotal(), cart.getTax(cart.getSubtotal())));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void checkoutEmptyCartTest(){
+        cart.remove(testItem1.getName());
+        cart.remove(testItem2.getName());
+        cart.checkout();
     }
 
 }
